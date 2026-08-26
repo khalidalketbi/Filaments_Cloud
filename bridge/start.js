87fd1670e4afff7e64fd230040cc9b6222150bb8
khalidalketbi <136941729+khalidalketbi@users.noreply.github.com>
2026-08-26 20:39:21 +0400

    Start Bambu print path worker

--- bridge/start.js
+++ bridge/start.js
@@ -2,4 +2,5 @@ import './index.js';
 import './camera-worker.js';
 import './usage-worker.js';
 import './print-state-cleaner.js';
+import './print-path-worker.js';
 import './external-print-import.js';
