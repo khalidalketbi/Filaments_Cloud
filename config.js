e29bdc5524ee11efcabf669356a9b125079226e6
khalidalketbi <136941729+khalidalketbi@users.noreply.github.com>
2026-08-26 17:47:43 +0400

    Load smooth Bambu camera renderer

--- config.js
+++ config.js
@@ -16,6 +16,7 @@ window.APP_CONFIG = {
     await load('./dashboard-pro.js');
     await load('./printer-enhancements.js');
     await load('./bambu-remote.js');
+    await load('./bambu-camera-smooth.js');
     await load('./bambu-safari-pairing.js');
     await load('./bambu-control-center.js');
     await load('./bambu-quick-temp.js');
