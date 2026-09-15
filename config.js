7e3e310931e99aeb1963914cb56844bd74844292
khalidalketbi <136941729+khalidalketbi@users.noreply.github.com>
2026-09-15 18:48:58 +0400

    Load project ETA dashboard module

--- config.js
+++ config.js
@@ -58,6 +58,7 @@ window.APP_CONFIG = {
     await load('./dashboard-pro.js');
     await load('./production-project-switcher.js');
     await load('./production-dashboard.js');
+    await load('./production-project-eta.js');
     await load('./production-tracking.js');
     await load('./production-all-projects.js');
     await load('./production-manual-time.js');
